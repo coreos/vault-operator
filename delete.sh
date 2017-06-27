b02ae9736ebd759ea951ee25063190a51366283c
Brandon Philips <brandon.philips@coreos.com>
2017-06-26 21:58:00 -0700

    delete: fixup script

--- delete.sh
+++ delete.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kubectl delete secret etcd-server-client-tls etcd-server-peer-tls operator-etcd-client-tls vault-client-tls vault-server-tls
 kubectl delete configmap vault
-kubectl delete -f manifests/etcd-cluster.yaml manifests/vault.yaml
+kubectl delete -f manifests/etcd-cluster.yaml -f manifests/vault.yaml
