0ed9b28d45bba6782730f8bb697f5b0127604bb0
Xiang Li <xiangli.cs@gmail.com>
2017-07-25 13:06:41 -0700

    hack: add a dev deployment yaml (#18)

--- cmd/vault-operator/main.go
+++ cmd/vault-operator/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"context"
+	"runtime"
+
+	"github.com/coreos-inc/vault-operator/pkg/operator"
+
+	"github.com/Sirupsen/logrus"
+)
+
 func main() {
 	// nothing interesting
+	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+
+	v := operator.New()
+	err := v.Start(context.TODO())
+	if err != nil {
+		logrus.Infof("operator stopped with %v", err)
+	}
 }
