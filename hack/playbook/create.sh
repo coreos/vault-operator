6137805b37e3e3fc787af493424a98665e155992
Hongchao Deng <hongchaodeng1@gmail.com>
2017-08-01 15:15:35 -0700

    *: add readiness probe to vault pod

--- hack/playbook/create.sh
+++ hack/playbook/create.sh
@@ -8,3 +8,6 @@
 kubectl create configmap example-vault-config --from-file=$PWD/hack/playbook/vault.hcl
 # Assume vault operator is running
 kubectl create -f $PWD/example/example_vault.yaml
+
+# Use the following command to port-forward vault pod
+#   kubectl get po -l app=vault -o jsonpath='{.items[*].metadata.name}' | xargs -0 -I {} kubectl port-forward {} 8200
