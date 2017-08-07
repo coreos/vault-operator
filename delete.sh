80619f59f27d3b3db42fdcb85b58ec1efc866d2a
Hongchao Deng <hongchaodeng1@gmail.com>
2017-08-07 15:31:44 -0700

    *: remove initial scripts

--- delete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-kubectl delete secret etcd-server-client-tls etcd-server-peer-tls operator-etcd-client-tls vault-client-tls vault-server-tls vault-etcd-client-tls
-kubectl delete configmap vault
-kubectl delete -f manifests/etcd-cluster.yaml -f manifests/vault.yaml
