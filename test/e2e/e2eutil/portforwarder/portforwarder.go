d93f961b554f41a3013f7566ba04d77335da954c
hasbro17 <hasbro17@gmail.com>
2017-08-29 18:12:37 -0400

    e2e: add portforwarding util to init and unseal vault

---
+++ test/e2e/e2eutil/portforwarder/portforwarder.go
@@ -0,0 +1,90 @@
+package portforwarder
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+)
+
+type PortForwarder interface {
+	// StartForwarding starts the client-go portforwarder to listen and forward ports to the specified pod
+	// Each port string maps a local port to the target pod's port and is of the format: "<local-port>:<pod-port>"
+	StartForwarding(podName, namespace string, ports []string) error
+	// StopForwarding stops the client-go portforwarder from forwarding ports to the specified pod
+	StopForwarding(podName, namespace string) error
+}
+
+type portForwarder struct {
+	activePods map[string]chan struct{}
+	kubeClient kubernetes.Interface
+	transport  http.RoundTripper
+	upgrader   spdy.Upgrader
+}
+
+// New returns a PortForwarder that uses client-go's implementation of the httpstream.Dialer interface
+// See https://github.com/kubernetes/client-go/blob/master/transport/spdy/spdy.go
+func New(kubeClient kubernetes.Interface, config *restclient.Config) (PortForwarder, error) {
+	transport, upgrader, err := spdy.RoundTripperFor(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get roundtripper: %v", err)
+	}
+	return &portForwarder{
+		activePods: map[string]chan struct{}{},
+		kubeClient: kubeClient,
+		transport:  transport,
+		upgrader:   upgrader,
+	}, nil
+}
+
+func (pf *portForwarder) StartForwarding(podName, namespace string, ports []string) error {
+	_, ok := pf.activePods[getNamespacedName(podName, namespace)]
+	if ok {
+		return fmt.Errorf("Already portforwarding to the pod (%v). Stop that first", getNamespacedName(podName, namespace))
+	}
+
+	url := pf.kubeClient.CoreV1().RESTClient().Post().Resource("pods").Namespace(namespace).Name(podName).SubResource("portforward").URL()
+
+	dialer := spdy.NewDialer(pf.upgrader, &http.Client{Transport: pf.transport}, "POST", url)
+	stopChan := make(chan struct{})
+	readyChan := make(chan struct{})
+	errChan := make(chan error)
+
+	k8sPF, err := portforward.New(dialer, ports, stopChan, readyChan, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create a : %v", err)
+	}
+
+	go func() {
+		errChan <- k8sPF.ForwardPorts()
+	}()
+
+	select {
+	case err = <-errChan:
+		return fmt.Errorf("failed to forward ports: %v", err)
+	case <-readyChan:
+	}
+
+	pf.activePods[getNamespacedName(podName, namespace)] = stopChan
+	return nil
+}
+
+func (pf *portForwarder) StopForwarding(podName, namespace string) error {
+	stopChan, ok := pf.activePods[getNamespacedName(podName, namespace)]
+	if !ok {
+		return fmt.Errorf("No ports being forwarded to the pod (%v)", getNamespacedName(podName, namespace))
+	}
+
+	// Stop the client-go port forwarder for this pod
+	close(stopChan)
+	delete(pf.activePods, getNamespacedName(podName, namespace))
+	return nil
+}
+
+func getNamespacedName(name, namespace string) string {
+	return path.Join(namespace, name)
+}
